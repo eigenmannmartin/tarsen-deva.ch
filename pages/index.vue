572a6530eef6f934dea167865bd65db2016b2167
Martin Eigenmann <github@eigenmannmartin.ch>
2017-12-29 15:12:41 +0100

    add some text

--- pages/index.vue
+++ pages/index.vue
@@ -12,28 +12,39 @@
       </div>
     </section>
     <section id="header" class="bg-primary">
+      <div class="text-center">
+        <h2 class="text-white">
+           bürgernah, vielfältig und dienstleistungsorientiert
+        </h2>
+      </div>
     </section>
 
-    <b-navbar sticky toggleable="md" type="light" variant="light">
+    <b-navbar sticky toggleable="md" type="light" variant="primary">
       <div class="container">
         <b-navbar-nav class="w-100">
           <div class="row w-100">
             <div class="col text-center">
-              <b-nav-item href="#">
-                <icon name="heartbeat" class="icon"></icon>
-                Motiviation
+              <b-nav-item href="#motivation">
+                <span class="text-white">
+                  <icon name="heartbeat" class="icon"></icon>
+                  Motiviation
+                </span>
               </b-nav-item>
             </div>
             <div class="col text-center">
-              <b-nav-item href="#">
-                <icon name="globe" class="icon"></icon>
-                Agenda
+              <b-nav-item href="#engagement">
+                <span class="text-white">
+                  <icon name="globe" class="icon"></icon>
+                  Agenda
+                </span>
               </b-nav-item>
             </div>
             <div class="col text-center">
-              <b-nav-item href="#">
-                <icon name="user" class="icon"></icon>
-                Profil
+              <b-nav-item href="#profile">
+                <span class="text-white">
+                  <icon name="user" class="icon"></icon>
+                  Profil
+                </span>
               </b-nav-item>
             </div>
           </div>
@@ -43,19 +54,102 @@
 
     <section id="motivation" class="section pt-5">
       <div class="container">
-        <h1 class="text-primary">Politik <small class="text-muted">Motivation</small></h1>
+        <div class="row">
+          <div class="col">
+            <h1 class="text-primary">Politik <small class="text-muted">Motivation</small></h1>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col">
+            <p>
+              Der Mensch steht für mich im Mittelpunkt. Mein grösstes Anliegen ist es, die Bedürfnisse der Schweizer Bevölkerung so gut wie möglich zu erfüllen. Dafür setze ich mich mit Kopf, Herz und vollem Tatendrang entschlossen ein.
+            </p>
+            <p>
+              Seit 2011 bin ich Mitglieder der CVP Schweiz sowie der JCVP Kanton SG. Innerhalb unserer Partei habe bereits sämtliche Vorstandsaufgaben ausgeübt wie Vizepräsident der Regionalpartei JCVP St. Gallen – Gossau - Rorschach, Delegierter JCVP Kanton SG oder auch Stellvertretungsfunktionen von internationalen Kongressen vom Internationalen Sekretär der JCVP Schweiz.
+            </p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 offset-md-3 col-md-6 col-xl-4 offset-xl-4">
+            <img class="w-100" src="~/assets/smartspider.png" />
+          </div>
+        </div>
+
+        <div class="row mt-5">
+          <div class="col">
+            <h6>Bildungspolitik <small class="text-muted">Die Stärke der Schweiz sind ihre wissbegierigen Leute</small></h6>
+            <p>
+              Wir sind wirklichkeitsnah, neugierig, praktisch, präzise und haben die Fähigkeit, das Wünschenswerte zu wollen und das Machbare zu planen.
+            </p>
+            <p>
+              Lassen Sie mich es unzweideutig und glasklar sagen: Bildung, Forschung und Innovation sind unsere wichtigsten, ja einzigen Rohstoffe von globaler Bedeutung. Geschliffen werden diese Talente durch gute Schulen, Fachhochschulen und Universitäten. Wettbewerbsfähig werden sie durch grenzüberschreitendes Messen mit Konkurrenten und die Möglichkeit zum Austausch. Darum stehe ich unmissverständlich ein für qualitativ hochstehende Aus- und Weiterbildung und für Chancengleichheit im internationalen Wettbewerb. Leistungsbereitschaft und Ehrgeiz sind unsere Stärken!
+            </p>
+          </div>
+        </div>
+
+        <div class="row mt-5">
+          <div class="col">
+            <h6>Familienpolitik <small class="text-muted">Für eine erfolgreiche Schweiz braucht es starke Familien</small></h6>
+            <p>
+              Die Gründung einer Familie darf nicht zu Armut führen. Gute Rahmenbedingungen für die Familien und den Mittelstand bilden einen Erfolgsfaktor für die Schweiz. Familien benötigen Geld, Zeit und Infrastrukturen. Mit der CVP-Volksinitiative „Für Ehe und Familie – gegen die Heiratsstrafe“, die voraussichtlich im Februar 2016 zur Abstimmung kommt, soll die Diskriminierung der Ehe gegenüber den anderen Lebensformen konsequent beseitigt werden. Die Vereinbarkeit von Beruf und Familie ist heute für viele junge Eltern von zentraler Bedeutung. Dem ist Rechnung zu tragen. Es braucht flächendeckende Angebote für die familienexterne Kinderbetreuung. Die traditionelle Familie darf dabei jedoch nicht benachteiligt werden. Die verschiedenen Familienformen sind heute Tatsache und sollen auch gelebt werden können. Auch die Väter sollen nach der Geburt ihres Kindes die Möglichkeit erhalten, ohne finanzielle Einbussen die Mütter zu unterstützen. Die Einführung eines zweiwöchigen Vaterschafsurlaubs, der von der EO kostenneutral bezahlt werden soll, ist vertretbar und kann finanziert werden. Auch soll unsere Gesellschaft wieder vermehrt ermuntert werden, sich im Sinne einer gelebten Hilfsbereitschaft gegenseitig zu unterstützen und von den gegenseitigen Erfahrungen und Möglichkeiten zu profitieren. Auch die moderne Gesellschaft braucht Solidarität und ehrenamtliches Engagement.
+            </p>
+          </div>
+        </div>
+
+        <div class="row mt-5">
+          <div class="col">
+            <h6>Migrationspolitik <small class="text-muted">Humanität und rechtsstaatlichkeit sind gleichermassen bedeutend</small></h6>
+            <p>
+              Deshalb heisst es: Bedrohten Menschen ist ohne Wenn und Aber Asyl zu gewähren. Kommt jemand zwar aus einer gewissen Perspektivlosigkeit, aber ohne Bedrohungssituation zu uns, gilt es klar zu machen, dass der richtige Weg einzig der Aufbau der Heimat ist. Auch das gehört zu einem humanitären Staat. Hier braucht es schnelle und dadurch faire Verfahren.
+            </p>
+            <p>
+              Der Grossteil der Menschen, die heute in die Schweiz kommen, sind nicht Flüchtlinge aus Kriegsgebieten, sondern Arbeiter aus EU-Staaten, mit denen wir über ein Freizügigkeitsabkommen verfügen. Die Wirtschaft ist auf diese Arbeitskräfte angewiesen. Gleichwohl gilt es, darauf zu achten, dass das Bevölkerungswachstum durch Zuwanderung nur so hoch ist, dass unsere Infrastrukturen und unsere Ressourcen nicht darunter leiden.
+            </p>
+          </div>
+        </div>
+
+        <div class="row mt-5">
+          <div class="col">
+            <h6>Aussenpolitik <small class="text-muted">Der bilaterale Weg mit der EU ist nicht der einfachste, aber der erfolgreichste</small></h6>
+            <p>
+              Deshalb ist er weiter zu gehen. Gleichzeitig ist dem anderen Weg, sprich dem Beitritt zur EU, eine Absage zu erteilen. Ich bin deshalb für einen Rückzug des Beitrittsgesuchs.
+            </p>
+            <p>
+              Die humanitäre Tradition der Schweiz soll auch in Zukunft eine weltweite Visitenkarte sein. Deshalb stehe ich ein für eine transparente Entwicklungshilfe und dafür, dass die guten Dienste der Schweiz auch in Zukunft dem Frieden in der Welt nützen.
+            </p>
+          </div>
+        </div>
+
+
       </div>
     </section>
     <section id="engagement" class="section pt-5">
       <div class="container">
-        <h1 class="text-primary">Agenda <small class="text-muted">Engagement</small></h1>
+        <div class="row">
+          <div class="col">
+            <h1 class="text-primary">Agenda <small class="text-muted">Engagement</small></h1>
+          </div>
+        </div>
       </div>
     </section>
     <section id="profile" class="section pt-5">
       <div class="container">
-        <h1 class="text-primary">Über mich <small class="text-muted">Profil</small></h1>
+        <div class="row">
+          <div class="col">
+            <h1 class="text-primary">Über mich <small class="text-muted">Profil</small></h1>
+          </div>
+        </div>
       </div>
     </section>
+
+    <footer class="bg-primary">
+      <div class="container">
+        <div class="row">
+          <div class="col">asdf</div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -70,7 +164,7 @@ $label-height: 60px;
 $bar-height: 56px;
 
 #header {
-  min-height: calc(100vh - #{$bar-height} - #{$label-height});
+  height: calc(100vh - #{$bar-height} - #{$label-height});
 }
 
 #label {
@@ -92,7 +186,7 @@ $bar-height: 56px;
 
   div {
     margin-top: -3px;
-    margin-left: 20px;
+    margin-left: 40px;
   }
 }
 
