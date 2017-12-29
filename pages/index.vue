25a3de744a3359f6a9bc489c4000e71273ef0e8d
Martin Eigenmann <github@eigenmannmartin.ch>
2017-12-29 17:27:30 +0100

    add profile image

--- pages/index.vue
+++ pages/index.vue
@@ -158,7 +158,7 @@
           </div>
         </div>
         <div class="row">
-          <div class="col">
+          <div class="col-12 col-md-9">
             <p>
               Mein Name lautet Latharsan Devasakayam und ich stamme ursprünglich aus Sri Lanka.
               Seit 1993 lebe ich mit meiner Familie in der Schweiz bzw. in St. Gallen.
@@ -169,6 +169,9 @@
               Regierungsrat Josef (Joe) Keller (CVP) immer wieder von der Schweizer Politik angesprochen.
               Durch diese lehrreichen Gespräche entdeckte ich die Leidenschaft für die Schweizer Politik.
             </p>
+          </div>
+          <div class="col-12 col-md-3 col-lg-2 offset-lg-1">
+            <img src="~/assets/tarsen-thinking.jpg" class="w-100" />
           </div>
         </div>
 
