34ec33b1f2f35b17512dc9b4b218e891062c4a08
Martin Eigenmann <github@eigenmannmartin.ch>
2017-12-29 11:39:06 +0100

    add base design

---
+++ nuxt.config.js
@@ -0,0 +1,69 @@
+const pkg = require('./package')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#3B8070' },
+
+  /*
+  ** Global CSS
+  */
+  css: ['~bootstrap.scss'],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: ['~plugins/vue-awesome.js'],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [, ['bootstrap-vue/nuxt', { css: false }]],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    vendor: ['vue-awesome'],
+
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      if (ctx.isServer) {
+        config.externals = [
+          nodeExternals({
+            whitelist: [/^vue-awesome/]
+          })
+        ]
+      }
+
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <section id="label">
+      <div class="container my-2">
+        <div>
+          <img class="cvp-logo" src="~/static/logo-csp.jpg"/>
+        </div>
+        <div class="logo-text">
+          <h1 class="d-inline">Tarsen Deva</h1><br />
+          <div class="text-muted">Latarsan Devasakayam</div>
+        </div>
+      </div>
+    </section>
+    <section id="header" class="bg-primary">
+    </section>
+
+    <b-navbar sticky toggleable="md" type="light" variant="light">
+      <div class="container">
+        <b-navbar-nav class="w-100">
+          <div class="row w-100">
+            <div class="col text-center">
+              <b-nav-item href="#">
+                <icon name="heartbeat" class="icon"></icon>
+                Motiviation
+              </b-nav-item>
+            </div>
+            <div class="col text-center">
+              <b-nav-item href="#">
+                <icon name="globe" class="icon"></icon>
+                Agenda
+              </b-nav-item>
+            </div>
+            <div class="col text-center">
+              <b-nav-item href="#">
+                <icon name="user" class="icon"></icon>
+                Profil
+              </b-nav-item>
+            </div>
+          </div>
+        </b-navbar-nav>
+      </div>
+    </b-navbar>
+
+    <section id="motivation" class="section pt-5">
+      <div class="container">
+        <h1 class="text-primary">Politik <small class="text-muted">Motivation</small></h1>
+      </div>
+    </section>
+    <section id="engagement" class="section pt-5">
+      <div class="container">
+        <h1 class="text-primary">Agenda <small class="text-muted">Engagement</small></h1>
+      </div>
+    </section>
+    <section id="profile" class="section pt-5">
+      <div class="container">
+        <h1 class="text-primary">Über mich <small class="text-muted">Profil</small></h1>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style lang="scss">
+$label-height: 60px;
+$bar-height: 56px;
+
+#header {
+  min-height: calc(100vh - #{$bar-height} - #{$label-height});
+}
+
+#label {
+  height: $label-height;
+}
+
+.cvp-logo {
+  height: calc(#{$label-height} - 5px);
+  float: left;
+}
+
+.logo-text {
+  height: $label-height;
+  padding-left: 80px;
+
+  h1 {
+    line-height: 2rem;
+  }
+
+  div {
+    margin-top: -3px;
+    margin-left: 20px;
+  }
+}
+
+.section {
+  min-height: 100vh;
+}
+
+.icon {
+  height: 100%;
+  margin-bottom: -2px;
+}
+</style>
